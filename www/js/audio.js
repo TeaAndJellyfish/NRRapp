05ee077850bfee7f8ced2373b23682032d51bd03
TeaAndJellyfish <tajcoffee@gmail.com>
2015-04-24 19:06:05 +0200

    Base

---
+++ www/js/audio.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+	// Stockage des éléments HTML
+		//le morceau mp3
+		var $zik	= $('#audio')[0];
+	
+		//controls
+		var $play 	= $('.play') ;
+		var $pause 	= $('.pause') ;
+		var $stop 	= $('.stop') ;
+	
+	
+	// Evénements
+		$play.click( jouer );
+		$pause.click( attente );
+		$stop.click( arret );
+	
+	
+	// Fonctions
+		function jouer(){			
+			$zik.play();
+		}	
+	
+		function attente(){
+			$zik.pause();
+		}
+	
+		function arret(){
+			$zik.pause();
+			$zik.currentTime = 0;
+		}
+	
+});
